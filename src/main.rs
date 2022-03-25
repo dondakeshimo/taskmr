79209465458161d4e14c5a62a522837f07d8a62d
dondakeshimo <went.went.takkun135@gmail.com>
2022-03-26 00:24:29 +0900

    feat: save to file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
+use dirs;
 use rusqlite::Connection;
+use std::fs;
+use std::process;
 
 use taskmr::infra::sqlite::task_repository::TaskRepository;
 use taskmr::presentation::command::cli::Cli;
 use taskmr::usecase::add_task_usecase::AddTaskUseCase;
 
 fn main() {
-    let task_repository = TaskRepository::new(Connection::open_in_memory().unwrap());
-    task_repository.create_table_if_not_exists().unwrap();
+    let mut db_file_path = dirs::config_dir().unwrap_or_else(|| {
+        eprintln!("Couldn't find out config directory.");
+        process::exit(1)
+    });
+    db_file_path.push("taskmr");
+    fs::create_dir_all(&db_file_path).unwrap_or_else(|err| {
+        eprintln!(
+            "Couldn't create taskmr directory in your config directory: {}",
+            err
+        );
+        process::exit(1)
+    });
+    db_file_path.push("taskmr.db");
+
+    let task_repository =
+        TaskRepository::new(Connection::open(&db_file_path).unwrap_or_else(|err| {
+            eprintln!("Couldn't connect your task database: {}", err);
+            process::exit(1)
+        }));
+
+    task_repository
+        .create_table_if_not_exists()
+        .unwrap_or_else(|err| {
+            eprintln!("Failed to create tables on your database: {}", err);
+            process::exit(1)
+        });
+
     let add_task_usecase = AddTaskUseCase::new(Box::new(task_repository));
     let cli = Cli::new(add_task_usecase);
     cli.handle();
