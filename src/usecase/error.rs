c0c21d7e5a9a1619df92236b7466426151622d11
dondakeshimo <went.went.takkun135@gmail.com>
2022-04-03 17:29:25 +0900

    feat: add AlreadyClosed error

--- src/usecase/error.rs
+++ src/usecase/error.rs
@@ -4,6 +4,8 @@ use thiserror::Error;
 pub enum UseCaseError {
     #[error("the task for id `{0}` is not found")]
     NotFound(i64),
+    #[error("the task for id `{0}` has already be closed")]
+    AlreadyClosed(i64),
 }
 
 #[cfg(test)]
@@ -15,6 +17,14 @@ mod tests {
         assert_eq!(
             UseCaseError::NotFound(2).to_string(),
             "the task for id `2` is not found".to_owned()
+        );
+    }
+
+    #[test]
+    fn test_already_closed() {
+        assert_eq!(
+            UseCaseError::AlreadyClosed(3).to_string(),
+            "the task for id `3` has already be closed".to_owned()
         );
     }
 }
